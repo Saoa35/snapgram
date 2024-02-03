3c80c63a59e26b543423abcf6bf9abf4047730ac
Saoa35 <saoamah@gmail.com>
2024-02-03 21:52:06 +0100

    * upgrated talwind.config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./globals.css";
 
 function App() {
-  return <div>APP</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
