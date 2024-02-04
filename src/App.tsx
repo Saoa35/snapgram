d92b0ab9d8313044f6f0cc6d698089b700574c1b
Saoa35 <saoamah@gmail.com>
2024-02-04 19:37:29 +0100

    + added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Routes } from "react-router-dom";
 import "./globals.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes></Routes>
+    </main>
+  );
 }
 
 export default App;
