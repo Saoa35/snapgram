e8ff0e2d30fb45193c5d0268c5f552a448c96b60
Saoa35 <saoamah@gmail.com>
2024-02-04 19:54:47 +0100

    + created Signin, Signup, Home & AuthLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import Home from "./_root/pages/Home";
 
 function App() {
   return (
     <main className="flex h-screen">
-      <Routes></Routes>
+      <Routes>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route index element={<Home />} />
+      </Routes>
     </main>
   );
 }
