bd97056f46bee0dec8d5fe1accb06027b953af69
Saoa35 <saoamah@gmail.com>
2024-02-05 16:11:01 +0100

    * upgraded tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import Home from "./_root/pages/Home";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
 
 function App() {
   return (
     <main className="flex h-screen">
       <Routes>
         <Route path="/sign-in" element={<SigninForm />} />
-        <Route index element={<Home />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
